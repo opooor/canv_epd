561c7f0ee591cd7122d38708ae574245ab1371bc
Hym <105768239+opooor@users.noreply.github.com>
2026-06-06 14:27:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const targetUrl = "https://hymmaker.my.canvasite.cn/";
+    const targetUrl = "https://khsj.cn/a9x3p31vq25n8lp/";
     const resp = await fetch(targetUrl, { redirect: "follow" });
     const headers = new Headers(resp.headers);
     // 删除防iframe嵌套头部
